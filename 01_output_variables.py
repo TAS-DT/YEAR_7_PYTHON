7a95b61dc4ce0901c7303f84299dccce2d056858
MrJonRoberts <jroberts@tas.qld.edu.au>
2024-04-16 10:06:44 +1000

    Create 01_output_variables.py
    01 Comments Added

---
+++ 01_output_variables.py
@@ -0,0 +1,63 @@
+# 01 Output and Variables
+
+
+# OUTPUT#######################################################################
+# 1. printing text
+
+
+# 2. combining text
+
+
+# 3. printing numbers
+
+
+# 4. printing quotation marks
+
+
+# VARIABLES ##################################################################
+
+# 1. create a variable
+
+
+# 2. create a string variable
+
+
+# 3. create a numeric variable
+
+
+# 4. create a boolean variable
+
+
+# OUTPUT VARIABLES ############################################################
+
+# 1. Print a string
+
+
+# 2. Print a number
+
+
+# 3. print the result of a calculation
+
+
+# 4. combine a some text and a string
+
+
+# 5. combine text and a number
+
+
+# 6. output mulitple lines of text.
+
+
+# more information.
+#  
+#  https://tascairns.sharepoint.com/sites/LearningPython/SitePages/Python-01-.aspx
+#  
+
+
+
+
+
+
+
+
+
