235256d566b9a0a62296f55a0686118b191c0172
MrJonRoberts <jroberts@tas.qld.edu.au>
2024-04-16 12:25:57 +1000

    7A
    7A

--- 01_output_variables_complete.py
+++ 01_output_variables_complete.py
@@ -4,15 +4,34 @@
 # OUTPUT#######################################################################
 # 1. printing text
 
+print("Hello World!")
+
+# errors
+
+# mismatch quotation marks.
+# print('hello World") # error SyntaxError: EOL while scanning string literal
+
+# wrong function name
+# write("Hello World!") # NameError: name 'write' is not defined
+
+# no closing bracket
+# print("Hello WOrld"  #SyntaxError: unexpected EOF while parsing
 
 # 2. combining text
 
+print("Harry" + "Potter")  # no space between
+print("Harry " + "Potter")
+print("Harry", "Potter")
 
 # 3. printing numbers
+
+print("13") # not a number - it is '1' '3' 
 
 
 # 4. printing quotation marks
 
+# print("He said "Hi"") #SyntaxError: invalid syntax
+print('He said "Hi"')
 
 # VARIABLES ##################################################################
 
